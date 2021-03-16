644a891398b07f00195cea7fad007e76f26eeb4a
Thepnathi <thepnathi@hotmail.com>
2021-03-16 13:37:53 +0000

    Update Classification.py to handle new DatasetHand

--- Classification.py
+++ Classification.py
@@ -1,51 +1,53 @@
 # Part 3 - Use the binary perceptron to train classifiers to discriminate between classes
 
 import numpy as np
-from DatasetLoader import DatasetLoader
+from DatasetHandler import DatasetHandler
 from Perceptron import Perceptron
-# https://numpy.org/doc/stable/reference/generated/numpy.sign.html
 
 class PerceptronClassification:
-    def predict(self, dataRow, bias, weight):
+    def predictict(self, dataRow, bias, weight):
         activation = np.dot(dataRow, weight) + bias
         return np.sign(activation)
 
-    def predict_test_dataset(self, test_data, test_label, bias, weight):
-        correct_predict = 0
+    # Given a test dataset with feature + label array. Returns the total accuracy for correct prediction
+    def compute_test_dataset_accuracy(self, test_data, test_label, bias, weight):
+        correct_predictict = 0
         for i, row in enumerate(test_data):
-            activation_score = self.predict(row, bias, weight)
+            activation_score = self.predictict(row, bias, weight)
             if activation_score == test_label[i]:
-                correct_predict += 1
-        accuracy = (correct_predict / len(test_data)) * 100
-        print(f'Accuracy - {accuracy}%')
+                correct_predictict += 1
+        return (correct_predictict / len(test_data)) * 100
 
 if __name__ == "__main__":
     # Loads the randomised dataset with two classes or all
-    dataloader = DatasetLoader()
-    # Data between class 1 and class 2
-    train_data1, train_label1 = dataloader.extract_two_classes_from_dataset('train.data', 1, 2, randomise=False)
-    test_data1, test_label1 = dataloader.extract_two_classes_from_dataset('test.data', 1, 2)
-    # Data between class 2 and class 3
-    train_data2, train_label2 = dataloader.extract_two_classes_from_dataset('train.data', 2, 3, randomise=False)
-    test_data2, test_label2 = dataloader.extract_two_classes_from_dataset('test.data', 2, 3)
-    # # Data between class 1 and class 3
-    train_data3, train_label3 = dataloader.extract_two_classes_from_dataset('train.data', 1, 3, randomise=False)
-    test_data3, test_label3 = dataloader.extract_two_classes_from_dataset('test.data', 1, 3)
 
-    # Initialise perceptron with start weight of 0 and bias of 0
+     # Test and Train Dataset for class 1 and class 2
+    train1 = DatasetHandler().extract_two_classes_from_dataset('train.data', 1, 2, randomise=False)
+    test1 = DatasetHandler().extract_two_classes_from_dataset('test.data', 1, 2, randomise=False)
+
+    # Test and Train Dataset for class 2 and class 3
+    train2 = DatasetHandler().extract_two_classes_from_dataset('train.data', 2, 3, randomise=False)
+    test2 = DatasetHandler().extract_two_classes_from_dataset('test.data', 2, 3, randomise=False)
+
+    # Test and Train Dataset between class 1 and class 3
+    train3 = DatasetHandler().extract_two_classes_from_dataset('train.data', 1, 3, randomise=False)
+    test3 = DatasetHandler().extract_two_classes_from_dataset('test.data', 1, 3, randomise=False)
+
+    # Initialise perceptron algorithm with start weight of 0 and bias of 0
     perceptron = Perceptron(0, 0)
 
-    # Training the perceptron with 20 iterations
-    bias1, weight1 = perceptron.train_perceptron(train_data1, train_label1, 20)
-    bias2, weight2 = perceptron.train_perceptron(train_data2, train_label2, 20)
-    bias3, weight3 = perceptron.train_perceptron(train_data3, train_label2, 20)
+    # Training the perceptron with 20 iterations for the three types of train dataset we have loaded
+    bias1, weight1 = perceptron.train_perceptron(train1.feature_dataset, train1.label_dataset, 20)
+    bias2, weight2 = perceptron.train_perceptron(train2.feature_dataset, train2.label_dataset, 20)
+    bias3, weight3 = perceptron.train_perceptron(train3.feature_dataset, train3.label_dataset, 20)
 
     # Initialise classification class to compute accuracy of test data
-    pred = PerceptronClassification()
+    predict = PerceptronClassification()
 
-    print("Test dataset with class 1 and 2")
-    pred.predict_test_dataset(test_data1, test_label1, bias1, weight1)
-    print("Test dataset with class 2 and 3")
-    pred.predict_test_dataset(test_data2, test_label2, bias2, weight2)
-    print("Test dataset with class 1 and 3")
-    pred.predict_test_dataset(test_data3, test_label3, bias3, weight3)
+    acc1 = predict.compute_test_dataset_accuracy(test1.feature_dataset, test1.label_dataset, bias1, weight1)
+    acc2 = predict.compute_test_dataset_accuracy(test2.feature_dataset, test2.label_dataset, bias2, weight2)
+    acc3 = predict.compute_test_dataset_accuracy(test3.feature_dataset, test3.label_dataset, bias3, weight3)
+
+    print(f"Test dataset with class 1 and 2 with accuracy of {acc1}%")
+    print(f"Test dataset with class 2 and 3 with accuracy of {acc2}%")
+    print(f"Test dataset with class 1 and 3 with accuracy of {acc3}%")
